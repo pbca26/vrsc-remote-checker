68bb4f17a77ac871ce7ee01421ffb3bd11162b0f
pbca26 <pbca26@gmail.com>
2018-07-29 11:01:47 +0300

    show approx blocks a day

--- power.js
+++ power.js
@@ -17,7 +17,7 @@ Promise.all(config.map((item, index) => {
 				})
 				.on('data', (data) => {
 					fs.writeFileSync(`miner-${index + 1}.txt`, data + '\n', { encoding: 'utf-8', flag: 'a' });
-					
+
 					// console.log('STDOUT: ' + data);
 				})
 				.stderr.on('data', (data) => {
@@ -37,20 +37,23 @@ Promise.all(config.map((item, index) => {
 .then(promiseResult => {
 	let totalHashrate = 0;
 	let networkhashps = 0;
+	let totalTimeToMine = 0;
 
 	for (let i = 0; i < config.length; i++) {
 		const rawData = fs.readFileSync(`miner-${i + 1}.txt`, { encoding: 'utf-8' });
-		const split1 = rawData.split(config[i].parse);
+		const split1 = rawData.split(config[i].parse || '{');
 		const split2 = split1[1].split('logout');
     split2[0] = '{' + split2[0];
 		const hashrate = JSON.parse(split2[0].trim())['localhashps'];
 		const timeToMine = (JSON.parse(split2[0].trim())['networkhashps'] / JSON.parse(split2[0].trim())['localhashps']) * (60 / 3600);
-		
+
 		networkhashps = JSON.parse(split2[0].trim())['networkhashps'];
 		totalHashrate += Number(hashrate);
-		console.log(`miner-${i + 1} hash rate: ${hashrate} | avg time to mine ${timeToMine.toFixed(1)}h`);		
+		totalTimeToMine += Number(timeToMine.toFixed(1));
+		console.log(`miner-${i + 1} hash rate: ${hashrate} | avg time to mine ${timeToMine.toFixed(1)}h`);
 	}
 
 	console.log('----------------\nTotal: ' + (totalHashrate / 1000000000).toFixed(3) + ' GH');
 	console.log('Total network rate: ' + (networkhashps / 1000000000).toFixed(3) + ' GH');
+	console.log('Total blocks a day: ' + ((totalTimeToMine / config.length) / 24).toFixed(1));
 });
